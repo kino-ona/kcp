866a6aba622a2da3aed15159a0bce201f121537f
youngmi <yeomia@gmail.com>
2020-09-01 20:39:04 +0900

    gulp4 setting

--- gulp_setting/gulpfile.babel.js
+++ gulp_setting/gulpfile.babel.js
@@ -1,14 +1,13 @@
 import gulp from 'gulp'
 import babel from 'gulp-babel'
-import eslint from 'gulp-eslint'
 import base64 from 'gulp-base64'
 import sass from 'gulp-sass'
 import autoprefixer from 'gulp-autoprefixer'
 import fileinclude from 'gulp-file-include'
+import webserver from 'gulp-webserver'
+import livereload from 'gulp-livereload'
 import del from 'del'
-import browserSync from 'browser-sync'
 
-const server = browserSync.create()
 const clean = () => {
   return del(['dist'])
 }
@@ -51,11 +50,10 @@ function style() {
     )
     .pipe(autoprefixer())
     .pipe(gulp.dest(dist, {sourcemaps: '/maps'}))
-    .pipe(browserSync.stream())
 }
 
 function htmlInclude() {
-  return gulp.src(paths.html).pipe(fileinclude(htmlIncludeOptions)).pipe(gulp.dest(dist)).pipe(browserSync.stream())
+  return gulp.src(paths.html).pipe(fileinclude(htmlIncludeOptions)).pipe(gulp.dest(dist))
 }
 
 function scripts() {
@@ -69,27 +67,26 @@ function scripts() {
     .pipe(gulp.dest(dist))
 }
 
-// function lintBase() {
-//   return gulp.src(paths.js).pipe(eslint()).pipe(eslint.format()).pipe(eslint.failAfterError())
-// }
-
 function copyImage() {
   return gulp.src(paths.image, {since: gulp.lastRun(copyImage)}).pipe(gulp.dest(dist))
 }
 
-function watchFiles(done) {
-  server.init({
-    server: './dist/',
-    open: false
-  })
-  done()
-  gulp.watch(paths.scss, style).on('change', browserSync.reload)
-  gulp.watch(paths.html, htmlInclude).on('change', browserSync.reload)
+function server() {
+  return gulp.src(dist).pipe(
+    webserver({
+      livereload: true
+    })
+  )
+}
+function watchFiles() {
+  livereload.listen()
+  gulp.watch(paths.scss, style)
+  gulp.watch(paths.html, htmlInclude)
   gulp.watch(paths.js, scripts)
-  gulp.watch(paths.image, copyImage)
+  gulp.watch(dist + '/**').on('change', livereload.changed)
 }
 
-const watch = gulp.series(watchFiles)
+const watch = gulp.parallel(server, watchFiles)
 const build = gulp.series(clean, gulp.parallel(style, scripts, htmlInclude, copyImage))
 
 exports.build = build
